fa4a5ebd055acb35020cbda2df54dc9087c3bb7b
csevere <carla.severe@gmail.com>
2018-01-22 15:55:23 -0500

    updated db creds

---
+++ src/config.php
@@ -0,0 +1,20 @@
+<?php
+
+<?php
+
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+
+
+    return (object) array(
+        'host' => $server,
+        'user' => $username,
+        'pass' => $password,
+        'dbname' => $db
+    ); 
+
+?>
+?>
